20d97d11862f63504276e14fd34fa18d0ce854f5
filodoks <filodoks@gmail.com>
2020-08-07 20:16:47 +0200

    07.08.20

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,16 @@ import React from "react";
 
 import Aux from "../../hoc/Auxiliary";
 import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = (props) => (
-    <Aux>
-        <div>Toolbar, SideBrawer, Backdrop</div>
-        <main className={classes.Content}>{props.children}</main>
-    </Aux>
+	<Aux>
+		<Toolbar />
+		<SideDrawer />
+		<div>Toolbar, SideBrawer, Backdrop</div>
+		<main className={classes.Content}>{props.children}</main>
+	</Aux>
 );
 
 export default layout;
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import classes from "./SideDrawer.css";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Auxiliary";
+
+const sideDrawer = (props) => {
+	return (
+		<Aux>
+			<Backdrop show />
+			<div className={classes.SideDrawer}>
+				<div className={classes.Logo}>
+					<Logo />
+				</div>
+				<nav>
+					<NavigationItems />
+				</nav>
+			</div>
+		</Aux>
+	);
+};
+
+export default sideDrawer;
+
+//32. Reusing the Backdrop 3:20
